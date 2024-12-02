298a5e5375e6e5363d33b4952cd8e31f1131def7
Adriel Poon <149524970+Dafilipinospork@users.noreply.github.com>
2024-12-02 16:21:08 -0500

    Update main.cpp
    erm added options filter

--- main.cpp
+++ main.cpp
@@ -59,12 +59,71 @@ vector<Anime> parseAnimeData(const string& filename) {
     }
     return animeList;
 }
+void displayAnimeInfo(const Anime& anime) {
+    cout << "Choose the information you want to view:" << endl;
+    cout << "1. Rating" << endl;
+    cout << "2. Episode Count" << endl;
+    cout << "3. Genre" << endl;
+    cout << "4. Type" << endl;
+    cout << "5. Community Size" << endl;
+    cout << "Enter your choice (1-5): ";
+    int choice;
+    cin >> choice;
+    if (choice == 1) {
+        cout << "Rating: " << anime.rating << endl;
+    }
+    else if (choice == 2) {
+        cout << "Episodes: " << anime.episodes << endl;
+    }
+    else if (choice == 3) {
+        cout << "Genre: ";
+        for (const auto& genre : anime.genres) {
+            cout << genre << " ";
+        }
+        cout << endl;
+    }
+    else if (choice == 4) {
+        cout << "Type: " << anime.type << endl;
+    }
+    else if (choice == 5) {
+        cout << "Community Size: " << anime.members << endl;
+    }
+    else {
+        cout << "Invalid choice!" << endl;
+    }
+}
 int main() {
     string filename = "Anime.csv";
     vector<Anime> animeList = parseAnimeData(filename);
-    for (const auto& anime : animeList) {
-        cout << "Anime: " << anime.name
-             << ", Rating: " << anime.rating << endl;
+    while (true) {
+        string animeName;
+        cout << "Enter the anime name (or type 'exit' to quit): ";
+        cin.ignore();  // To clear any leftover newline character
+        getline(cin, animeName);
+        // Exit if user types "exit"
+        if (animeName == "exit") {
+            break;
+        }
+        // Search for the anime by name
+        Anime* foundAnime = nullptr;
+        for (auto& anime : animeList) {
+            if (anime.name == animeName) {
+                foundAnime = &anime;
+                break;
+            }
+        }
+        if (foundAnime) {
+            displayAnimeInfo(*foundAnime);
+        } else {
+            cout << "Anime not found!" << endl;
+        }
+        // Ask the user if they want to search for another anime
+        cout << "Do you want to search for another anime? (yes/no): ";
+        string response;
+        cin >> response;
+        if (response != "yes") {
+            break;
+        }
     }
     return 0;
 }
