1609910bbaabd9c75caba5dd72cc628f8dea20fe
s11374832 <adrielspoon@gmail.com>
2024-12-03 02:24:34 -0500

    Here's the Display genre types + finisheed what I think I have to do lmk wot else to do

---
+++ cmake-build-debug/CMakeFiles/DSAFinal.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSAFinal.dir/main.cpp.obj"
+  "CMakeFiles/DSAFinal.dir/main.cpp.obj.d"
+  "DSAFinal.exe"
+  "DSAFinal.exe.manifest"
+  "DSAFinal.pdb"
+  "libDSAFinal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSAFinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DSAFinal.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DSAFinal.
+# This may be replaced when dependencies are built.
